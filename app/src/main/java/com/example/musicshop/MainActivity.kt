81b7822217b8d6b7398f25045d4366f85743a930
Nirwash <nirwash@bk.ru>
2022-04-21 21:04:41 +0300

    инициализация и реализация spinner

--- app/src/main/java/com/example/musicshop/MainActivity.kt
+++ app/src/main/java/com/example/musicshop/MainActivity.kt
@@ -12,7 +12,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var b: ActivityMainBinding
     var quantity = 0
     val items = arrayOf("Guitar", "Keyboard", "Ukulele", "Drums")
-
+    val goodsMap = mapOf("Guitar" to 750, "Keyboard" to 1000, "Ukulele" to 500, "Drums" to 1500)
+    lateinit var goodsName: String
+    var price = 0
+    var image = R.drawable.guitar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,31 +28,59 @@ class MainActivity : AppCompatActivity() {
         b.bMinus.setOnClickListener {
             b.tvCount.text = decreaseQuantity().toString()
         }
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+
+        createSpinner()
+    }
+
+    private fun createSpinner() {
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         b.spinner.apply {
             adapter = arrayAdapter
-            onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(
                     parent: AdapterView<*>?,
                     view: View?,
                     position: Int,
                     id: Long
                 ) {
-                    Toast.makeText(this@MainActivity, "Selected ${items[position]}", Toast.LENGTH_SHORT).show()
+                    getPrice()
+                    setItemImage(goodsName)
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("Not yet implemented")
+                    getPrice()
                 }
             }
         }
     }
+
     private fun increaseQuantity(): Int {
         quantity++
+        getPrice()
         return quantity
-    }private fun decreaseQuantity(): Int {
-        if(quantity > 0) quantity--
+    }
+
+    private fun decreaseQuantity(): Int {
+        if (quantity > 0) quantity--
+        getPrice()
         return quantity
+    }
+
+    private fun getPrice() {
+        goodsName = b.spinner.selectedItem.toString()
+        price = goodsMap[goodsName]!!
+        b.tvCurrentPrice.text = (quantity * price).toString()
+    }
+
+    private fun setItemImage(goodsName: String) {
+        image = when (goodsName) {
+            "Keyboard" -> R.drawable.piano
+            "Ukulele" -> R.drawable.ukulele
+            "Drums" -> R.drawable.drums
+            else -> R.drawable.guitar
+        }
+        b.imgItem.setImageResource(image)
     }
 
 

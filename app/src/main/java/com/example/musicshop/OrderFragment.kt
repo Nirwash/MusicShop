d6e5bc57ab68fc902c76f8a4790b1cf3230cf476
Nirwash <nirwash@bk.ru>
2022-04-24 16:46:02 +0300

    реализация отправки заказа по email

--- app/src/main/java/com/example/musicshop/OrderFragment.kt
+++ app/src/main/java/com/example/musicshop/OrderFragment.kt
@@ -1,5 +1,9 @@
 package com.example.musicshop
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +17,9 @@ import com.example.musicshop.databinding.FragmentOrderBinding
 class OrderFragment: BaseFragment() {
     private var _binding: FragmentOrderBinding? = null
     private val b get() = _binding!!
+    private val email = arrayOf("Nirwash@bk.ru")
+    private val subjectEmail = "Order from music shop"
+    lateinit var emailText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +32,7 @@ class OrderFragment: BaseFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentOrderBinding.inflate(inflater, container, false)
         return b.root
     }
@@ -37,11 +44,33 @@ class OrderFragment: BaseFragment() {
             fm.popBackStack()
         }
         val order: Order = model.getData().value!!
-        b.tvOrder.text = order.toString()
+        emailText = "Customer: ${order.customerName}\n" +
+                "Goods: ${order.goodsName}\n" +
+                "Quantity: ${order.quantity}\n" +
+                "Price: ${order.orderPrice}$\n" +
+                "Total: ${order.quantity * order.orderPrice}$"
+        b.tvCustomerName.text = order.customerName
+        b.tvGoodsName.text = order.goodsName
+        b.tvQuantityCount.text = order.quantity.toString()
+        b.tvPriceCount.text = order.orderPrice.toString()
+        b.tvOrderPriceCount.text = (order.quantity * order.orderPrice).toString()
+        b.btnSubmitOrder.setOnClickListener { composeEmail(email, subjectEmail, emailText) }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun composeEmail(addresses: Array<String>, subject: String, text: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:") // only email apps should handle this
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, text)
+        }
+        if (context?.packageManager?.let { intent.resolveActivity(it) } != null) {
+            startActivity(intent)
+        }
     }
 }
